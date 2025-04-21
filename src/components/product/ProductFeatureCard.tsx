5e7ebfb5deb0ec012cf3977b81659225137ddfec
Sahil Popli <vatsintechsolution@gmail.com>
2025-04-22 02:52:13 +0530

    just to push the push

--- src/components/product/ProductFeatureCard.tsx
+++ src/components/product/ProductFeatureCard.tsx
@@ -49,7 +49,7 @@ export const ProductFeatureCard: FC<ProductFeatureCardProps> = ({
         </div>
         <h3 className="text-xl md:text-2xl lg:text-3xl font-bold mb-2 text-white dark:text-white">{title}</h3>
         <p className="text-sm md:text-lg text-white dark:text-white/80">{description}</p>
-      </div>
+      </div> 
     </motion.div>
   );
 };
